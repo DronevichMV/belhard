e34a01cf1a1e1abf9cf13da2f37741423c59c1dc
DronevichMV <mary.com119@gmail.com>
2022-06-20 23:26:14 +0300

    add print 2

--- 3_3.py
+++ 3_3.py
@@ -2,4 +2,6 @@ name = input("Enter your name: ")
 age = input("Enter your age: ")
 city = input("Enter your city: ")
 print("Hello, " + name + "! " + "Your age is " + age + ". " + "You live in " + city)
+print("Hello, %s! You're %s. Your city is %s" % (name, age, city))
+
 
