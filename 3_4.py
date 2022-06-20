b1047fddb2145a8fdbf57722d6425ada1c00590b
DronevichMV <mary.com119@gmail.com>
2022-06-21 01:04:34 +0300

    add variables 2

--- 3_4.py
+++ 3_4.py
@@ -1,4 +1,8 @@
 number_1 = input("Enter number 1: ")
 number_2 = input("Enter number 2: ")
 number_3 = input("Enter number 3: ")
+ostatok_1 = int(number_1) % 2
+ostatok_2 = int(number_2) % 2
+ostatok_3 = int(number_3) % 2
+
 
