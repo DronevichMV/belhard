0123f0d689bba90dfd70c774b10f1a1e85c316c8
DronevichMV <mary.com119@gmail.com>
2022-06-20 22:59:50 +0300

    add print

--- 3_2.py
+++ 3_2.py
@@ -1,6 +1,8 @@
 number_1 = input("Enter number 1: ")
 number_2 = input("Enter number 2: ")
 number_3 = input("Enter number 3: ")
+average = (float(number_1) + float(number_2) + float(number_3)) / 3
+print(round(average, 3))
 
 
 
