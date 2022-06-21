128e2cb93dd0e85747df22e745336018130d091a
DronevichMV <mary.com119@gmail.com>
2022-06-21 17:05:41 +0300

    add print

--- 3_4.py
+++ 3_4.py
@@ -1,8 +1,8 @@
 number_1 = input("Enter number 1: ")
 number_2 = input("Enter number 2: ")
 number_3 = input("Enter number 3: ")
-ostatok_1 = int(number_1) % 2
-ostatok_2 = int(number_2) % 2
-ostatok_3 = int(number_3) % 2
+print("положительных: " + str((int(number_1) > 0) + (int(number_2) > 0) + (int(number_3) > 0)))
+print("отрицательных: " + str((int(number_1) < 0) + (int(number_2) < 0) + (int(number_3) < 0)))
+
 
 
