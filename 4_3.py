d252837b05a67a568b83c929f039fa6f88867184
DronevichMV <mary.com119@gmail.com>
2022-06-22 23:09:48 +0300

    add print

--- 4_3.py
+++ 4_3.py
@@ -1,5 +1,4 @@
-slovar_1 = {'name':input("Enter your name: "), 'email':input("Enter your email: ")}
 n = int(input("Enter last number: "))
-slovar_2 = {i:slovar_1 for i in range(0, n, 1)}
+slovar_2 = {i:{'name':input("Enter your name: "), 'email':input("Enter your email: ")} for i in range(0, n, 1)}
 print(slovar_2)
 
