ad82a3e5185c7301a6761d6db1663be408cbe65f
DronevichMV <mary.com119@gmail.com>
2022-06-22 15:48:46 +0300

    add dict

--- 4_2.py
+++ 4_2.py
@@ -2,5 +2,8 @@ sent = input("Enter your sentence: ")
 sent = sent.replace(" ", "")
 spisok = list(sent)
 mnozh = set(spisok)
+slovar = dict.fromkeys(mnozh, 0)
+for mnozh in spisok:
+    slovar[mnozh] += 1
 
 
