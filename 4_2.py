7f39f97f89cec24b607235ad5ea7d43db049998f
DronevichMV <mary.com119@gmail.com>
2022-06-22 22:57:18 +0300

    add print

--- 4_2.py
+++ 4_2.py
@@ -1,9 +1,5 @@
 sent = input("Enter your sentence: ")
 sent = sent.replace(" ", "")
-spisok = list(sent)
-mnozh = set(spisok)
-slovar = dict.fromkeys(mnozh, 0)
-for mnozh in spisok:
-    slovar[mnozh] += 1
+slovar = {i:sent.count(i) for i in sent}
 print(slovar)
 
