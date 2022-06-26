3949adbe13b082a864a103d79bd555957c769f4d
DronevichMV <mary.com119@gmail.com>
2022-06-26 23:42:03 +0300

    add if

--- 5_2.py
+++ 5_2.py
@@ -1,4 +1,11 @@
 f_number = int(input("Enter first number: "))
 s_number = int(input("Enter second number: "))
 operation = input("Enter a sign: ")
-
+if operation == "+":
+    print(f_number + s_number)
+elif operation == "-":
+    print(f_number - s_number)
+elif operation == "*":
+    print(f_number * s_number)
+elif operation == "/":
+    print(f_number / s_number)
