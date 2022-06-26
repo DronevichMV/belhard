edb10d609b8ec338402fe78a8115f838c522686f
DronevichMV <mary.com119@gmail.com>
2022-06-26 23:23:39 +0300

    add print

--- 5_1.py
+++ 5_1.py
@@ -1,6 +1,8 @@
 N = int(input("Enter first number: "))
 M = int(input("Enter second number: "))
 K = int(input("Enter third number: "))
-
+for i in range (N+1):
+    if i % M == 0 and i > K:
+        print(i)
 
 
