7abc1407608f54ade912dccb14b7a07b702caebc
DronevichMV <mary.com119@gmail.com>
2022-06-29 16:33:30 +0300

    add if

--- 5_3.py
+++ 5_3.py
@@ -2,10 +2,10 @@ N = int(input("Enter last number: "))
 
 count = 0
 for i in range (2, N+1, 2):
-    if count < 5:
-        print(i, end=" ")
-        count += 1
-    else:
+    if count >= 5:
         print()
         print(i, end=" ")
         count = 1
+    else:
+        print(i, end=" ")
+        count += 1
