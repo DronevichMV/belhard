5fd73e3be67716a00d57c5159b6a542bc5eab0d0
DronevichMV <mary.com119@gmail.com>
2022-07-08 20:16:15 +0300

    add def

--- 6_1.py
+++ 6_1.py
@@ -1,1 +1,13 @@
-¥¦¨¬ ¢ë¢®¤  ª®¬ ­¤ ­  íªà ­ (ECHO) ¢ª«îç¥­.
+def decimal_to_bin(number: int) -> str:
+    bin_number: str = ""
+    while number // 2 > 0:
+        bin_number += str(number % 2)
+        number //= 2
+    bin_number += str(number)
+    bin_number = bin_number[::-1]
+    return bin_number
+    print(bin_number)
+
+
+
+
