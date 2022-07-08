946cc8b5086893f1f21ac868868d4a805a699e87
DronevichMV <mary.com119@gmail.com>
2022-07-08 21:06:11 +0300

    add def

--- 6_3.py
+++ 6_3.py
@@ -1,1 +1,15 @@
-def 
+def offset(n: int, spisok: list) -> list:
+    if N > 0:
+        for _ in range(N):
+            spisok.insert(0, spisok.pop(-1))
+    else:
+        for _ in range(abs(N)):
+            spisok.append(spisok.pop(0))
+    return spisok
+    print(spisok)
+
+
+
+
+
+
