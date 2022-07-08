48badf518df8e1acb3a9aee217ba772d169c3897
DronevichMV <mary.com119@gmail.com>
2022-07-08 21:38:41 +0300

    add def

--- 6_5.py
+++ 6_5.py
@@ -1,1 +1,6 @@
-Режим вывода команд на экран (ECHO) включен.
+def rev(spisok: list) -> list:
+    for i in range(len(spisok) // 2):
+        j = (i + 1) * -1
+        spisok[i], spisok[j] = spisok[j], spisok[i]
+    return spisok
+
